37229d176df6dcda6e240f5d37bb5fd9180e6982
Ariyan Rumi <99718240+AriyanRumi@users.noreply.github.com>
2022-05-02 21:07:53 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const btn =document.querySelector('.btn')
+const wish =document.querySelector('.mainContent')
+const tips =document.querySelector('.tips')
+const audi =document.querySelector('#horror')
+
+btn.addEventListener('click',()=>{
+wish.classList.add('active')
+btn.style.display ='none'
+tips.style.display ='none'
+audi.play()
+audi.currentTime()
+})
